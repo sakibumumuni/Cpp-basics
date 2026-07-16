c95caec4296cf27ace5e733a0442bbbc76174c46
sakibumumuni <sakibumumuni285@gmail.com>
2026-07-16 20:49:42 +0000

    add: a cpp snippet that receives a user's input and decides whether to count upward or abort or the bature of the input based on the input. Can be used for a trouble shoting software

---
+++ cpp/countup01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*
+- This is a snippet that prompts a user to enter 'y' or 'Y' to proceed counting
+in increments of 1 and 'n' or 'N' to Abort.
+- When a user click on any other character apart from 'n', 'N', 'y' or 'Y', there
+is a message display which says "Bad input!"
+*/
+
+int main () 
+{
+    char x;
+    int counter;
+    while (counter >= 0)
+    {
+        cout << "Enter 'y' or 'Y' to continue and 'n' or 'N' to abort!!" << "\n";
+        cin >> x;
+        if (x == 'y' || x == 'Y')
+        {
+            counter++;
+            cout << counter << "\n";
+        }
+        else if (x == 'n' || x == 'N')
+        {
+            cout << "Abort!!" << "\n";
+            return false;
+        }
+        else 
+        {
+            cout << "Bad input!!" << "\n";
+            return false;
+        }
+    }
+    return 0;
+}
